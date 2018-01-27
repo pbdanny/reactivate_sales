ce3c3ad0b686ab96c7cea4063856077e16c6aa41
pbdanny <pb.danny@gmail.com>
2018-01-28 06:31:43 +0700

    git second commit

---
+++ munge/01-munge.R
@@ -0,0 +1,15 @@
+# Data manipulations
+# Highlight and run command below do load project without munging.
+# This is useful when debugging data manipulation code.
+# rm(list = ls()); library(ProjectTemplate); load.project(list(munging=FALSE)) 
+
+# It is often useful to create sets of variable names
+# By convention, the list that stores these variable sets is called 'v'
+# v <- list()
+
+# edit datafame name according to preferred style
+check_oss <- check.oss
+rm(check.oss)
+colnames(check_oss) <- make.names(colnames(check_oss))
+check_oss$OpenDate <- strptime(check_oss$OpenDate, format = "%d/%m/%Y %H:%M:%S")
+check_oss$OpenDate <- as.POSIXct(check_oss$OpenDate)
